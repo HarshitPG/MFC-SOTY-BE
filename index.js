987171480304325e7572daa21971af0c024e077c
Harshit P G <harshitpg2004@gmail.com>
2024-01-21 18:13:54 +0530

    Added vercel config

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
-app.use(express.json());
 app.use("/auth", AuthRoute);
 app.use("/users", userRoute);
 app.use("/questions", questionRoute);
