4cc28e86ffecb251e814aab03764f642389f38dc
Harshit P G <harshitpg2004@gmail.com>
2024-02-18 23:19:00 +0530

    Update validateTokenHandler.js

--- middleware/validateTokenHandler.js
+++ middleware/validateTokenHandler.js
@@ -11,14 +11,30 @@ const validateToken = async (req, res, next) => {
   }
 
   token = authHeader.split(" ")[1];
-
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECERT, (err, decoded) => {
-    if (err) {
-      return res.status(401).json({ message: "User is not authorized" });
-    }
-    req.user = decoded.user;
-    next();
-  });
+  try {
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECERT, (err, decoded) => {
+      if (err) {
+        return res.status(401).json({ message: "User is not authorized" });
+      }
+      if (!decoded || !decoded.id) {
+        // console.log(decoded);
+        console.log("Invalid token payload:", decoded);
+        return res.status(401).json({ message: "Invalid token payload" });
+      }
+      const userId = decoded.id;
+      if (req.params.id !== userId) {
+        console.log("req.params", req.params);
+        console.log("userId", userId);
+        return res
+          .status(403)
+          .json({ message: "Unauthorized access to user data" });
+      }
+      req.userId = userId;
+      next();
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 module.exports = validateToken;
