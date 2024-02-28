79fcde044d56550051fda828537f7f82d4613efc
Harshit P G <harshitpg2004@gmail.com>
2024-02-28 12:32:14 +0530

    minor update

--- controllers/questionController.js
+++ controllers/questionController.js
@@ -39,7 +39,7 @@ const getQuestions = async (req, res) => {
       });
 
       if (filteredQuestion.length === 0) {
-        return res.status(404).json({
+        return res.status(200).json({
           message: "No questions available for the specified difficulty level.",
         });
       }
